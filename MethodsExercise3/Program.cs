a4ed09cb7aa5175c9b43edce8b208b1440810f7f
Maverick Grayer <maverick72032@gmail.com>
2024-03-22 16:27:35 -0400

    Code cleanup

--- MethodsExercise3/Program.cs
+++ MethodsExercise3/Program.cs
@@ -7,11 +7,9 @@ namespace MethodsExercise3
     {
         static void Main(string[] args)
         {
-            //part 1-2 done with one method, two calls
             PrintToConsoleNumsInRange(-1000, 1000, 1);
             PrintToConsoleNumsInRange(3, 999, 3);
 
-            //part 3, ask info in main, check and respond in CheckIfIntsAreEqual method
             Console.WriteLine("Give me two integers, and I will tell you if they are the same.");
             Console.Write("Int 1: ");
             int questionableInt1 = Int32.Parse(Console.ReadLine());
@@ -19,32 +17,26 @@ namespace MethodsExercise3
             int questionableInt2 = Int32.Parse(Console.ReadLine());
             CheckIfIntsAreEqual(questionableInt1, questionableInt2);
 
-            //part 4,
             Console.WriteLine("Give me an integer, and I will tell you if it is even or odd.");
             Console.Write("Int: ");
             int oddOrEvenInt = Int32.Parse(Console.ReadLine());
             IsNumOddOrEven(oddOrEvenInt);
 
-            //part 5,
             Console.WriteLine("Give me an integer, and I will tell you if it is positive or negative.");
             Console.Write("Int: ");
             int posOrNegInt = Int32.Parse(Console.ReadLine());
             IsNumPosOrNeg(posOrNegInt);
 
-            //part 6
             Console.WriteLine("What is your age");
             Console.Write("Age: ");
             int Age = Int32.Parse(Console.ReadLine());
             CanVote(Age);
 
-            //Heatin' up
-            //part 7
             Console.WriteLine("Give me a integer, and I will tell you if it is in the range -10 to 10");
             Console.Write("Int: ");
             int userInputInt = Int32.Parse(Console.ReadLine());
             IsWithinRange(-10, 10, userInputInt);
 
-            //part 8
             Console.WriteLine("Give me a integer (1-12), and I will creat a multiplication table for it.");
             Console.Write("Int: ");
             int mTableInt = Int32.Parse(Console.ReadLine());
